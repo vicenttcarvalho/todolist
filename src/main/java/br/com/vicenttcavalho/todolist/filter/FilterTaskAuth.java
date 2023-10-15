7a01fbf4f006c21617a67f32b5cc9bbd5ea02a90
vicenttcarvalho <vicentecleyton@gmail.com>
2023-10-14 22:31:34 -0300

    Password Decode

--- src/main/java/br/com/vicenttcavalho/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/vicenttcavalho/todolist/filter/FilterTaskAuth.java
@@ -3,8 +3,12 @@ package br.com.vicenttcavalho.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.vicenttcavalho.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,6 +16,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -29,13 +36,22 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     String[] credentials = authString.split(":");
     String username = credentials[0];
     String password = credentials[1];
-    System.out.println("Authorization");
-    System.out.println(username);
-    System.out.println(password);
-    // Validar usuário
-    // Validar senha
 
-    filterChain.doFilter(request, response);
+    // Validar usuário
+    var user = this.userRepository.findByUsername(username);
+    if (user == null) {
+      response.sendError(401);
+    } else {
+      filterChain.doFilter(request, response);
+    }
+    // Validar senha
+    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+    if (passwordVerify.verified) {
+      filterChain.doFilter(request, response);
+    } else {
+      response.sendError(401);
+    }
+
   }
 
 }
