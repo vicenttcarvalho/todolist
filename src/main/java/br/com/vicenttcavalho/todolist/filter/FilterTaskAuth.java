8e4479d53d5b138cb5b501399db723f0bb2815b7
vicenttcarvalho <vicentecleyton@gmail.com>
2023-10-15 09:11:26 -0300

    Update Tarefa

--- src/main/java/br/com/vicenttcavalho/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/vicenttcavalho/todolist/filter/FilterTaskAuth.java
@@ -26,7 +26,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
     var servletPath = request.getServletPath();
 
-    if (servletPath.equals("/tasks/")) {
+    if (servletPath.startsWith("/tasks/")) {
 
       // Pegar autenticação (usuario e senha)
       var authorization = request.getHeader("Authorization");
--- src/main/java/br/com/vicenttcavalho/todolist/task/TaskController.java
+++ src/main/java/br/com/vicenttcavalho/todolist/task/TaskController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,4 +51,12 @@ public class TaskController {
     return tasks;
   }
 
+  // http://localhost:8080/tasks/546546853-fdbjhs-44587441
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    var idUser = request.getAttribute("idUser");
+    taskModel.setIdUser((UUID) idUser);
+    taskModel.setId(id);
+    return this.taskRepository.save(taskModel);
+  }
 }
