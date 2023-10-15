3c1ba5e75eedfe93ee28c4b3af21e5d8c532cb91
vicenttcarvalho <vicentecleyton@gmail.com>
2023-10-14 22:12:05 -0300

    Filtro Continuação

--- src/main/java/br/com/vicenttcavalho/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/vicenttcavalho/todolist/filter/FilterTaskAuth.java
@@ -1,24 +1,41 @@
 package br.com.vicenttcavalho.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
-
-import jakarta.servlet.Filter;
+import org.springframework.web.filter.OncePerRequestFilter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
 
-    System.out.println("Chegou no filtro");
-    chain.doFilter(request, response);
+    // Pegar autenticação (usuario e senha)
+    var authorization = request.getHeader("Authorization");
+
+    var authEncoded = authorization.substring("Basic".length()).trim();
+
+    byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+    var authString = new String(authDecode);
+
+    String[] credentials = authString.split(":");
+    String username = credentials[0];
+    String password = credentials[1];
+    System.out.println("Authorization");
+    System.out.println(username);
+    System.out.println(password);
+    // Validar usuário
+    // Validar senha
+
+    filterChain.doFilter(request, response);
   }
 
 }
